c7b51e734e31ef0e5d0ba15fbcceebb7e946e812
mariaortizz <ortizmariadelrosario@gmail.com>
2024-03-18 23:13:47 +0200

    avance proyecto ML flask terminado

--- src/app/app.py
+++ src/app/app.py
@@ -1,33 +1,18 @@
 '''Archivo por el que corre la aplicación'''
 from flask import Flask, render_template, request
 import os 
-from models import extraccion_modelos, extraccion_cluster
 from pathlib import Path
+import pandas as pd
+from models_funciones import extraccion_cluster, extraccion_escalado, extraccion_modelos\
+    , aplicar_escalado, transf_tipo_inmueble, aplicar_modelo
 
 ruta_modelos = Path(os.getcwd(), 'src', 'model', 'modelos_cluster_012.zip')
 ruta_cluster = Path(os.getcwd(), 'src', 'model', 'modelo_kmeans.pkl')
+ruta_escalado = Path(os.getcwd(), 'src', 'model', 'robust_scaler_model.pkl')
 
 MODELOS = extraccion_modelos(ruta_modelos)
 CLUSTER = extraccion_cluster(ruta_cluster)
-
-'''
-'balcon' : 1/0, 
-'armarios_empotrados' : 1/0, 
-'estacionamiento' : 1/0, 
-'trastero' : 1/0,
-'terraza' : 1/0, 
-'annio_construccion' : valor, 
-'metros_cuadrados' : valor, 
-'bannos' : valor, 
-'latitud' : valor, 
-'longitud' : valor, 
-'cee_e' : valor, 
-'zona_e' : valor,
-'tipo_inmueble_HouseType 1: Pisos' : 1/0,
-'tipo_inmueble_HouseType 2: Casa o chalet' : 1/0,
-'tipo_inmueble_HouseType 4: Dúplex' : 1/0,
-'tipo_inmueble_HouseType 5: Áticos' : 1/0
-'''
+ESCALER = extraccion_escalado(ruta_escalado)
 
 # os.chdir(os.path.dirname(__file__))
 
@@ -38,37 +23,40 @@ app.config.from_object("config")
 def home():
     return render_template("home.html")
 
-# @app.route('/predict', methods = ['POST'])
-# def predict(balcon, amrarios, estacionamiento, trastero, terraza, construccion, metros, 
-#             bannos, lat, long, cee, zona, piso, casa, duplex, atico):
-#     balcon = float(request.args.get('a', None))
-#     amrarios
-#     estacionamiento
-#     trastero
-#     terraza
-#     construccion
-#     metros
-#     bannos
-#     lat
-#     long
-#     cee
-#     zona
-#     piso
-#     casa
-#     duplex
-#     atico
-#     a = float(request.args.get('a', None))
-#     b = float(request.args.get('b', None))
-#     c = float(request.args.get('c', None))
-#     d = float(request.args.get('d', None))
-#     cluster = CLUSTER.predict([[balcon, amrarios, estacionamiento, trastero, terraza, 
-#                              construccion, metros, bannos, lat, long, cee, zona, 
-#                              piso, casa, duplex, atico]])
-#     prediccion = m.predict([[balcon, amrarios, estacionamiento, trastero, terraza, 
-#                              construccion, metros, bannos, lat, long, cee, zona, 
-#                              piso, casa, duplex, atico]])
-#     print(app.config['VARIABLE'])
-#     return render_template("predict.html")
+@app.route('/valorar')
+def valorar():
+    return render_template("valorar.html")
+
+
+@app.route('/predict', methods = ['POST', 'GET'])
+def predict():
+    if request.method == 'POST':
+
+        balcon = 1 if request.form['balcon'] == "SI" else 0
+        armarios = 1 if request.form['armarios'] == "SI" else 0
+        estacionamiento = 1 if request.form['estacionamiento'] == "SI" else 0
+        trastero = 1 if request.form['trastero'] == "SI" else 0
+        terraza = 1 if request.form['terraza'] == "SI" else 0
+        construccion = float(request.form['construccion'])
+        metros = float(request.form['metros'])
+        bannos = int(request.form['bannos'])
+        lat = float(request.form['lat'])
+        long = float(request.form['long'])
+        cee = float(request.form['cee'])
+        zona = float(request.form['zona'])
+        tipo_inmueble = request.form['tipo_inmueble']
+
+        piso, casa, duplex, atico = transf_tipo_inmueble(tipo_inmueble)
+
+        construccion_e, metros_e = aplicar_escalado(construccion, metros, ESCALER)
+
+        precio = aplicar_modelo(CLUSTER, MODELOS, balcon, armarios, estacionamiento, 
+                                trastero, terraza, construccion_e, metros_e, bannos, 
+                                lat, long, cee, zona, piso, casa, duplex, atico)
+        
+        return render_template("predict.html", pecio_predd = precio[0].round(2))
+    else:
+        return "Metodo no permitido"
 
 if __name__ == "__main__":
     app.run(port=5000, debug=True)
---
+++ src/app/models_funciones.py
@@ -0,0 +1,126 @@
+import pickle
+import zipfile
+import traceback
+import inspect
+import numpy as np
+import pandas as pd
+
+
+def extraccion_modelos(ruta_modelos):
+    '''Función para extraer los modelos que tenemos cargados en un zip'''
+    try:
+        modelos = {}
+        with zipfile.ZipFile(ruta_modelos, 'r') as zip_ref:
+            for i,nombre_archivo in enumerate(zip_ref.namelist()):
+                with zip_ref.open(nombre_archivo) as archivo_pickle:
+                    model = pickle.load(archivo_pickle)
+                    modelos[i] = model
+        return modelos 
+    except Exception as a:
+        traceback.print_exc()
+        func = inspect.stack()[1].function
+        print(f"No se pudo terminar el proceso en la función {func} por {a}")
+
+def extraccion_cluster(ruta_cluster):
+    '''Función para extraer el cluster que tenemos en pickle'''
+    try:
+        with open(ruta_cluster, 'rb') as archivo:
+            km = pickle.load(archivo)
+        return km 
+    except Exception as a:
+        traceback.print_exc()
+        func = inspect.stack()[1].function
+        print(f"No se pudo terminar el proceso en la función {func} por {a}")
+
+def extraccion_escalado(ruta_escaler):
+    '''Función para extraer el cluster que tenemos en pickle'''
+    try:
+        with open(ruta_escaler, 'rb') as archivo:
+            km = pickle.load(archivo)
+        return km 
+    except Exception as a:
+        traceback.print_exc()
+        func = inspect.stack()[1].function
+        print(f"No se pudo terminar el proceso en la función {func} por {a}")
+
+def aplicar_escalado(nuevo_dato1, nuevo_dato2, escaler):
+    '''Funcion para escalar los datos que entran nuevos'''
+    try:
+        nuevos_datos = np.array([nuevo_dato1, nuevo_dato2]).reshape(1, -1)
+        datos_e = escaler.transform(nuevos_datos)
+        return datos_e[0][0], datos_e[0][1]
+    except Exception as a:
+        traceback.print_exc()
+        func = inspect.stack()[1].function
+        print(f"No se pudo terminar el proceso en la función {func} por {a}")
+
+def transf_tipo_inmueble(x):
+    '''Función para transformar los datos que recibimos'''
+    try:
+        valores_tipo_inmueble = {
+            "piso": 0,
+            "casa": 0,
+            "duplex": 0,
+            "atico": 0
+        }
+
+        valores_tipo_inmueble[x] = 1
+
+        piso = valores_tipo_inmueble['piso']
+        casa = valores_tipo_inmueble['casa']
+        duplex = valores_tipo_inmueble['duplex']
+        atico = valores_tipo_inmueble['atico']
+
+        return piso, casa, duplex, atico
+    except Exception as a:
+        traceback.print_exc()
+        func = inspect.stack()[1].function
+        print(f"No se pudo terminar el proceso en la función {func} por {a}")
+
+def aplicar_modelo(
+        CLUSTER, MODELOS, balcon, armarios, estacionamiento, trastero, 
+        terraza, construccion_e,metros_e, bannos, lat, long, cee, zona, 
+        piso, casa, duplex, atico
+        ):
+    '''Función para aplicar el cluster a los datos nuevos que tenemos'''
+    try:
+        cluster_pred = CLUSTER.predict([[
+            balcon, armarios, estacionamiento, trastero, 
+            terraza, construccion_e,metros_e, bannos, 
+            lat, long, cee, zona, piso, casa, duplex, atico
+            ]])
+
+        data = {'balcon' : [balcon],
+                            'armarios_empotrados' : [armarios], 
+                            'estacionamiento' : [estacionamiento], 
+                            'trastero' : [trastero],
+                            'terraza' : [terraza], 
+                            'annio_construccion' : [construccion_e], 
+                            'metros_cuadrados' : [metros_e],
+                            'bannos' : [bannos],
+                            'latitud' : [lat],
+                            'longitud' : [long],
+                            'cee_e' : [cee],
+                            'zona_e' : [zona],
+                            'tipo_inmueble_HouseType 1: Pisos' : [piso],
+                            'tipo_inmueble_HouseType 2: Casa o chalet' : [casa],
+                            'tipo_inmueble_HouseType 4: Dúplex' : [duplex],
+                            'tipo_inmueble_HouseType 5: Áticos' : [atico],
+                            'Cluster' : [cluster_pred[0]]
+        }
+        valores = pd.DataFrame(data)
+
+        model = MODELOS[cluster_pred[0]] 
+        print(model)
+        print(valores)
+        precio = model.predict(valores)
+        print(precio)
+        return precio
+    except Exception as a:
+        traceback.print_exc()
+        func = inspect.stack()[1].function
+        print(f"No se pudo terminar el proceso en la función {func} por {a}")
+
+
+
+
